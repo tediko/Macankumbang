2268d48be2f39e1c8410833a81cc5a204491cab9
tediko <tediko123@wp.pl>
2020-10-06 13:46:31 +0200

    Added mobile menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('hello');
+/* HEADER DROPDOWN MENU */
+const menuBtn = document.querySelector('.header__mobile');
+const menu = document.querySelector('.header__menu');
+
+function isOpen () {
+    if (menuBtn.classList.contains('header__mobile--active')) {
+        menuBtn.classList.remove('header__mobile--active');
+        menu.classList.remove('header__mobile--active');
+    } else {
+        menuBtn.classList.add('header__mobile--active');
+        menu.classList.add('header__mobile--active');
+
+    }
+}
+
+menuBtn.addEventListener('click', isOpen);
